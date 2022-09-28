daa2f21068380a9fced79ff5a134a068480c77d2
Mitankshi <114574805+Mitankshi@users.noreply.github.com>
2022-09-28 15:43:46 +0530

    Add files via upload

---
+++ pythonassignment1.py
@@ -0,0 +1,31 @@
+#Write a Python program to print "Hello Python"?
+
+print("Hello Python")
+
+#Write a Python program to do arithmetical operations addition and division.?
+
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+print(a+b)
+print(a/b)
+
+#Write a Python program to find the area of a triangle?
+
+b = int(input("enter base of triangle"))
+h = int(input("enter height of triangle"))
+print(1/2*b*h)
+
+#Write a Python program to swap two variables?
+
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c=a
+a=b
+b=c
+print("the value of first number after swapping is ", a)
+print("the value of second number after swapping is ", b)
+
+#Write a Python program to generate a random number?
+
+import random
+print(random.randint(0,100))
